ebd1a605ef0b4b20aa7a8401c5c295ac849a3db1
Kevin Conroy <Kcdrumming@gmail.com>
2021-05-17 12:57:55 -0700

    Added UpdateBookmark

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav(props) {
   return (
-    <nav className="Nav">
-      <Link to={"/"} />
-      Bookmark List <Link to={"/add-bookmark"}>Add Bookmark</Link>
+    <nav className='Nav'>
+      <Link to={'/'}>
+        Bookmark List
+      </Link>
+      {' '}
+      <Link to={'/add-bookmark'}>
+        Add Bookmark
+      </Link>
     </nav>
   );
 }
